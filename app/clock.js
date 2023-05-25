5d04cd1419f45c65147c13be1f081bace762357f
TickedOffCodess <132794877+TickedOffCodess@users.noreply.github.com>
2023-05-25 07:23:04 -0700

    Update GUI (#14)
    * Initial commit
    
    * Include package-lock in .gitignore
    
    * Remove App from app display name
    
    * Update GUI to increase contrast and readability
    
    * Update date to display above day value
    
    * Update x value for time

---
+++ app/clock.js
@@ -0,0 +1,39 @@
+import document from 'document';
+
+
+const UI_DAY_VALUE = document.getElementById('dayValue');
+const UI_DATE_VALUE = document.getElementById('dateValue');
+const UI_TIME_VALUE = document.getElementById('timeValue');
+const UI_AMPM_VALUE = document.getElementById('ampmValue');
+
+export function updateClock(currentDate) {
+
+    // const minutes = ;
+    // Set the time label with format hh:mm AM/PM
+    let hours = currentDate.getHours();
+    let mins = padWithZero(currentDate.getMinutes(), 2)
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; 
+  
+    UI_TIME_VALUE.text = `${hours}:${mins}`;
+    UI_AMPM_VALUE.text = `${ampm}`;
+  
+    const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    UI_DAY_VALUE.text = `${daysOfWeek[currentDate.getDay()]}`;
+      // Display formatted date as Mon May 8
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const day = padWithZero(currentDate.getDate(), 2);
+        // currentDate.getDate().toString().padStart(2, '0');
+    UI_DATE_VALUE.text = `${months[currentDate.getMonth()]} ${day}`;
+  
+  }
+
+  function padWithZero(number, length) {
+    let str = String(number);
+    while (str.length < length) {
+      str = '0' + str;
+    }
+    return str;
+  }
+  
--- app/index.js
+++ app/index.js
@@ -1,19 +1,16 @@
 import { HeartRateSensor } from 'heart-rate';
+import { BodyPresenceSensor } from "body-presence";
 import { me as appbit } from 'appbit';
 import { user } from 'user-profile';
 import { vibration } from 'haptics';
 import document from 'document';
-import { display } from 'display';
-import { today } from 'user-activity';
 import clock from 'clock';
+import { updateClock } from './clock';
 
 const heartRateLabel = document.getElementById('heartRateLabel');
-const dateLabel = document.getElementById('dateLabel');
-const timeLabel = document.getElementById('timeLabel');
-const rhrLabel = document.getElementById('rhrLabel');
-const atLabel = document.getElementById('atLabel');
+const rhrValue = document.getElementById('rhrValue');
+const atValue = document.getElementById('atValue');
 
-const middleRowBackground = document.getElementById('middleRowBackground');
 const gradientRectangleHeart = document.getElementById("gradientRectangleHeart");
 
 clock.granularity = 'minutes';
@@ -21,31 +18,20 @@ clock.granularity = 'minutes';
 clock.addEventListener('tick', (evt) => {
   // Set the date label
   let currentDate = evt.date;
-  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  const dayOfWeek = days[currentDate.getDay()];
-  // Display formatted date as Mon 5/8/23
-  const twoDigitYear = String(currentDate.getFullYear()).slice(-2);
-  const formattedDate = `${dayOfWeek} ${currentDate.getMonth() + 1}/${currentDate.getDate()}/${twoDigitYear}`;
-  dateLabel.text = formattedDate;
-  // Set the time label with format hh:mm AM/PM
-  const hours = currentDate.getHours();
-  const minutes = padWithZero(currentDate.getMinutes(), 2);
-  const ampm = hours >= 12 ? 'PM' : 'AM';
-  const hours12 = hours % 12 || 12;
-  timeLabel.text = `${hours12}:${minutes} ${ampm}`;
+  updateClock(currentDate);
 });
 
+const heartRateSensor = new HeartRateSensor();
 if (appbit.permissions.granted('access_heart_rate')) {
   if (HeartRateSensor) {
-    const heartRateSensor = new HeartRateSensor();
     heartRateSensor.addEventListener('reading', () => {
-      heartRateLabel.text = `${heartRateSensor.heartRate} BPM`;
+      heartRateLabel.text = `${heartRateSensor.heartRate}`;
       updateHeartRateZone(heartRateSensor.heartRate);
-      
+
       let rhr = user.restingHeartRate;
       let at = rhr + 15;
-      rhrLabel.text = `RHR: ${rhr}`;
-      atLabel.text = `AT: ${at}`;
+      rhrValue.text = `${rhr}`;
+      atValue.text = `${at}`;
 
       if (heartRateSensor.heartRate > at) {
         vibration.start('alert');
@@ -54,15 +40,30 @@ if (appbit.permissions.granted('access_heart_rate')) {
     heartRateSensor.start();
   } else {
     console.log('Heart Rate Sensor is not available');
+    heartRateLabel.text = "--";
   }
 } else {
   console.log('Permission to access heart rate data is not granted');
 }
 
+if (BodyPresenceSensor) {
+  const body = new BodyPresenceSensor();
+  body.addEventListener("reading", () => {
+    if (!body.present) {
+      heartRateSensor.stop();
+      heartRateLabel.text = "--";
+    } else {
+      heartRateSensor.start();
+    }
+  });
+  body.start();
+}
+
+
 function updateHeartRateZone(heartRate) {
   let rhr = user.restingHeartRate;
   let zoneColors;
-  
+
   if (heartRate < rhr + 6) {
     zoneColors = ['#99ccff', '#0033cc'];
   } else if (heartRate < rhr + 11) {
@@ -78,16 +79,3 @@ function updateHeartRateZone(heartRate) {
   gradientRectangleHeart.gradient.colors.c1 = zoneColors[0];
   gradientRectangleHeart.gradient.colors.c2 = zoneColors[1];
 }
-
-function padWithZero(number, length) {
-  let str = String(number);
-  while (str.length < length) {
-    str = '0' + str;
-  }
-  return str;
-}
-  
-
-  
-  
-  
