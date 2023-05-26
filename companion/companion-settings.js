f15131af62926e7e64a2236d0d2a2187123c0aad
tickedoffcodess <tickedoffcodess@gmail.com>
2023-05-26 09:54:51 -0700

    Add companion settings

---
+++ companion/companion-settings.js
@@ -0,0 +1,48 @@
+import * as messaging from 'messaging';
+import { settingsStorage } from 'settings';
+import { me as companion } from 'companion';
+
+const KEY_COLOR_MODE = 'colorMode';
+
+// Initialize
+export function initialize () {
+  settingsStorage.addEventListener('change', evt => {
+    // Which setting changed
+    console.log(`key: ${evt.key}`);
+
+    // What was the old value
+    console.log(`old value: ${evt.oldValue}`);
+
+    // What is the new value
+    console.log(`new value: ${evt.newValue}`);
+
+    // Send if value is different
+    if (evt.oldValue !== evt.newValue) {
+      sendValue(evt.key, evt.newValue);
+    }
+  })
+}
+
+function sendValue (key, val) {
+  if (val) {
+    sendSettingData({
+      key,
+      value: JSON.parse(val)
+    });
+  }
+}
+
+function sendSettingData (data) {
+  // If we have a MessageSocket, send the data to the device
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    messaging.peerSocket.send(data);
+  } else {
+    console.log('No peerSocket connection');
+  }
+}
+
+// Settings were changed while the companion was not running
+if (companion.launchReasons.settingsChanged) {
+  // Send the value of the setting
+  sendValue(KEY_COLOR_MODE, settingsStorage.getItem(KEY_COLOR_MODE));
+}
