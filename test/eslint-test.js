d75d1740deded3912b46aba3de1ec6504cc59d1d
tickedoffcodess <tickedoffcodess@gmail.com>
2023-06-01 08:32:08 -0700

    Add test for linting each file

---
+++ test/eslint-test.js
@@ -0,0 +1,43 @@
+import glob from 'glob';
+import ESLint from 'eslint';
+import { assert } from 'chai';
+import mocha from 'mocha';
+
+const paths = glob.sync('./+(app|test|companion)/**/*.js');
+const engine = new ESLint.ESLint({
+  useEslintrc: true
+});
+let results;
+async function main () {
+  results = await engine.lintFiles(paths);
+  results.forEach(result => {
+    console.log(`File: ${result.filePath}`);
+    console.log(`Error Count: ${result.errorCount}`);
+    console.log(`Warning Count: ${result.warningCount}`);
+  });
+  console.log(results);
+
+  mocha.describe('ESLint', function () {
+    results.forEach((result) => generateTest(result));
+  });
+}
+
+main();
+
+function generateTest (result) {
+  const { filePath, messages } = result;
+
+  mocha.it(`validates ${filePath}`, function () {
+    if (messages.length > 0) {
+      assert.fail(false, true, formatMessages(messages));
+    }
+  });
+}
+
+function formatMessages (messages) {
+  const errors = messages.map((message) => {
+    return `${message.line}:${message.column} ${message.message.slice(0, -1)} - ${message.ruleId}\n`;
+  });
+
+  return `\n${errors.join('')}`;
+}
