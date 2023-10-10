fd9d5284dbbb53d002a08acd6c3a9c5690bb2b5b
tickedoffcodess <tickedoffcodess@gmail.com>
2023-10-10 09:49:59 -0700

    Edit settings with alert type options

--- settings/index.jsx
+++ settings/index.jsx
@@ -10,11 +10,32 @@ function mySettings(props) {
       >
         <Slider
           label={`Alert Interval: ${props.settings.alertInterval || 0} seconds`}
-          settingsKey="alertInterval"
-          min="0"
-          max="300"
-          step="15"
+          settingsKey='alertInterval'
+          min='0'
+          max='300'
+          step='15'
         />
+      </Section>
+      <Section
+        title={<Text bold>Alert Type</Text>}
+        description={<Text>Set the alert type.</Text>}
+      >
+        <Select
+          selectViewTitle='Select Alert Type'
+          label='Alert Type'
+          settingsKey='alertType'
+          options={[
+            { name: 'Alert', value: 'alert' },
+            { name: 'Bump', value: 'bump' },
+            { name: 'Confirmation', value: 'confirmation' },
+            { name: 'Confirmation Max', value: 'confirmation-max' },
+            { name: 'Nudge', value: 'nudge' },
+            { name: 'Nudge Max', value: 'nudge-max' },
+            { name: 'Ping', value: 'ping' },
+            { name: 'Ring', value: 'ring' }
+          ]}
+          />
+        {}
       </Section>
       <Section
         title={<Text bold>Anaerobic Threshold (AT) Settings</Text>}
@@ -22,7 +43,7 @@ function mySettings(props) {
           <Select
             selectViewTitle='Select AT formula'
             label={'AT based on '}
-            settingsKey="atFormula"
+            settingsKey='atFormula'
             options={[
               { name: 'MaxHR x 50%', value: 'maxHR50' },
               { name: 'MaxHR x 55%', value: 'maxHR55' },
@@ -31,14 +52,14 @@ function mySettings(props) {
               { name: 'Custom AT', value: 'custom' }
             ]}
             />
-            {JSON.parse(props.settings.atFormula).values[0].value === 'custom' ? <TextInput label="Custom AT" settingsKey="customAT" type="number"/> : null}
+            {JSON.parse(props.settings.atFormula).values[0].value === 'custom' ? <TextInput label='Custom AT' settingsKey='customAT' type='number'/> : null}
           {}
       </Section>
       <Section
         title={<Text bold>HR Zone Background Settings</Text>}
         description={<Text>Set the background color mode for HR Zones to solid color (toggle ON) or gradient colors (toggle OFF).</Text>}>
           <Toggle
-              settingsKey="colorMode"
+              settingsKey='colorMode'
               label={`${props.settings.colorMode === 'true' ? 'ON: Solid Color' : 'OFF: Gradient Colors'}`}
           />
       </Section>
